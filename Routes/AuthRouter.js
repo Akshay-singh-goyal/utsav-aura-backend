33f8149838a3d1f9b04aa2425ca1d543e78229ab
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2025-10-17 14:40:52 +0530

    Update AuthRouter.js

--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -1,17 +1,26 @@
 import express from "express";
-import { signup, login, me, getUsers } from "../Controllers/AuthController.js";
+import {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  me,
+  getUsers,
+} from "../Controllers/authController.js";
+import { addProduct } from "../Controllers/ProductController.js";
 import { signupValidation, loginValidation } from "../Middlewares/AuthValidation.js";
 import { requireAuth, requireAdmin } from "../Middlewares/Auth.js";
-import { addProduct } from "../Controllers/ProductController.js";
 
 const router = express.Router();
 
 // --- Public routes ---
-router.post("/login", loginValidation, login);
 router.post("/signup", signupValidation, signup);
+router.post("/login", loginValidation, login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 // --- Protected routes ---
-router.get("/me", requireAuth, me);           // user profile
+router.get("/me", requireAuth, me); // user profile
 router.get("/getusers", requireAuth, requireAdmin, getUsers); // admin only
 
 // --- Admin product route ---
